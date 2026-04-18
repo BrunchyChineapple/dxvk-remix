8a0d4753743359a1e264d29c7e0608e6055c7bdc
Kim2091 <jpavatargirl@gmail.com>
2026-04-18 11:48:32 -0400

    diag: add temporary [RTX-Diag] logs to verify camera-validity skip hypothesis
    Throwaway instrumentation. Will be reverted once we have a runtime
    log showing whether injectRTX is silently skipping the RT block due
    to isCameraValid returning false.

--- src/dxvk/rtx_render/rtx_camera_manager.cpp
+++ src/dxvk/rtx_render/rtx_camera_manager.cpp
@@ -22,6 +22,8 @@
 #include "rtx_camera_manager.h"
 
 #include "dxvk_device.h"
+#include "../../util/util_string.h"
+#include "../../util/log/log.h"
 
 namespace {
   constexpr float kFovToleranceRadians = 0.001f;
@@ -179,8 +181,14 @@ namespace dxvk {
                                             const Matrix4& viewToProjection) {
     DecomposeProjectionParams decomposeProjectionParams = getOrDecomposeProjection(viewToProjection);
 
+    const uint32_t newFrame = m_device->getCurrentFrameId();
+    const uint32_t oldFrame = getCamera(type).getLastUpdateFrame();
+    Logger::warn(str::format(
+      "[RTX-Diag] processExternalCamera setting m_frameLastTouched=", newFrame,
+      " (was=", oldFrame, ") type=", (int)type));
+
     getCamera(type).update(
-      m_device->getCurrentFrameId(),
+      newFrame,
       worldToView,
       viewToProjection,
       decomposeProjectionParams.fov,
